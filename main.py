1f2f19a6f2bba05bda6499d97294c5e43bc6fa98
dylan-teehan-skc <dylan.teehan@gmail.com>
2025-03-11 22:49:20 +0000

    Refactor/ organise code into folders

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from tensorflow.keras.models import load_model
-from hand_detection import HandDetector
-from data_processor import DataProcessor
+from model_training.hand_detection import HandDetector
+from preprocessing.data_processor import DataProcessor
 
 def main():
-    # Example usage of hand detection
     model = load_model('hand_gesture_model.h5')
     detector = HandDetector(model=model)
     detector.detect_hands()
-
 
 if __name__ == "__main__":
     main()
